5861a113e9b796904d3aace23779ddada91dfce1
Eduu187 <gbminato48@gmail.com>
2026-02-15 14:26:00 -0300

    melhorando codigo

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,39 +1,16 @@
-resource "aws_lambda_function" "create_task" {
-  function_name    = "CreateTaskFunction"
+resource "aws_lambda_function" "lawyer_api" {
+  function_name    = "LawyerApiMonolith"
   role             = aws_iam_role.lambda_role.arn
   runtime          = "python3.10"
-  handler          = "src.handlers.create_handler.handler" 
+  
+  handler          = "src.handlers.main_handler.handler" 
+  
   filename         = "lambda_function_payload.zip"
   source_code_hash = fileexists("lambda_function_payload.zip") ? filebase64sha256("lambda_function_payload.zip") : null
-  environment { variables = { TABLE_NAME = var.table_name } }
-}
-
-resource "aws_lambda_function" "get_task" {
-  function_name    = "GetTaskFunction"
-  role             = aws_iam_role.lambda_role.arn
-  runtime          = "python3.10"
-  handler          = "src.handlers.get_handler.handler" 
-  filename         = "lambda_function_payload.zip"
-  source_code_hash = fileexists("lambda_function_payload.zip") ? filebase64sha256("lambda_function_payload.zip") : null
-  environment { variables = { TABLE_NAME = var.table_name } }
-}
-
-resource "aws_lambda_function" "update_task" {
-  function_name    = "UpdateTaskFunction"
-  role             = aws_iam_role.lambda_role.arn
-  runtime          = "python3.10"
-  handler          = "src.handlers.update_handler.handler" 
-  filename         = "lambda_function_payload.zip"
-  source_code_hash = fileexists("lambda_function_payload.zip") ? filebase64sha256("lambda_function_payload.zip") : null
-  environment { variables = { TABLE_NAME = var.table_name } }
-}
-
-resource "aws_lambda_function" "delete_task" {
-  function_name    = "DeleteTaskFunction"
-  role             = aws_iam_role.lambda_role.arn
-  runtime          = "python3.10"
-  handler          = "src.handlers.delete_handler.handler" 
-  filename         = "lambda_function_payload.zip"
-  source_code_hash = fileexists("lambda_function_payload.zip") ? filebase64sha256("lambda_function_payload.zip") : null
-  environment { variables = { TABLE_NAME = var.table_name } }
+  
+  environment {
+    variables = {
+      TABLE_NAME = var.table_name
+    }
+  }
 }
