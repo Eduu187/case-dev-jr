7a9faf3502da92329a979c3468c2f882437dfb63
Eduu187 <gbminato48@gmail.com>
2026-02-17 12:08:40 -0300

    alterando valor do output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "api_url" {
   description = "URL base da API"
-  value       = "${aws_api_gateway_deployment.deployment.invoke_url}/tasks"
+  value       = "${aws_api_gateway_stage.dev.invoke_url}/tasks"
 }
